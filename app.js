ef58297e702d74b72e888ccca9b6a386db294087
Jaloliddin7 <abdutolipovjaloliddin913@gmail.com>
2023-02-24 16:43:00 +0500

    ketti

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const mode = document.querySelector('.mode')
 const body = document.querySelector('body')
-// const mode = document.querySelector('.mode')
-// const mode = document.querySelector('.mode')
-// const mode = document.querySelector('.mode')
-// const mode = document.querySelector('.mode')
 
 mode.addEventListener('click' , ()=>{
     mode.classList.toggle('mode1')
