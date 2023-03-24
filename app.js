08e2d61f6418cf2a9f947f6d3d40889c13a07f69
Jaloliddin7 <abdutolipovjaloliddin913@gmail.com>
2023-03-24 07:39:33 +0500

    Ip manzilar kelyapti

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+// mode
 const mode = document.querySelector('.mode')
 const lightBtn = document.querySelector('.light-btn')
 const body = document.querySelector('body')
+
+// IP MANZIL
+const form = document.querySelector("#form")
+const input = document.querySelector(".input")
+const avatar = document.querySelector("#avatar")
 
 const local = localStorage.getItem("mode")
 
@@ -18,11 +24,30 @@ const Dark = () => {
 
 mode.addEventListener('click', () => {
     Dark()
-    localStorage.setItem("mode","dark")
+    localStorage.setItem("mode", "dark")
 })
 
 
 lightBtn.addEventListener('click', () => {
     Dark()
-    localStorage.setItem("mode","")
+    localStorage.setItem("mode", "")
 })
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let user = input.value
+
+    async function getData() {
+        const res = await fetch(`https://api.github.com/users/${user}`)
+        let Data = await res.json()
+
+        console.log(Data.avatar_url);
+
+        avatar.src = Data.avatar_url
+
+    }
+    getData()
+
+})
+
